2cfeaf604058a5e8fedc79d5a59373d5bb359abf
hellpoethero <hellpoethero1994@gmail.com>
2020-12-26 04:05:24 +0900

    update data

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 dt = pd.read_csv("https://raw.githubusercontent.com/hellpoethero/football/main/big5.csv", index_col=0)
 
 
@@ -26,16 +28,21 @@ def hello(name=None):
     return render_template('Polar Area Chart.html', name=name)
 
 
-@app.route('/data')
+@app.route('/data', methods=["GET"])
 def get_data():
     player_id = request.args.get('player_id')
     if player_id:
-        player_json = json.loads(dt.iloc[int(player_id)].to_json())
+        ids = player_id.split(",")
+        player_json = json.loads(dt.iloc[int(ids[0])].to_json())
         max_json = json.loads(dt[dt["minutes_90s"] >= 5].max().to_json())
-        return {
+        response = {
             "player": player_json,
             "max": max_json
         }
+        if len(ids) > 1:
+            compare_json = json.loads(dt.iloc[int(ids[1])].to_json())
+            response["compare"] = compare_json
+        return response
     else:
         return "Fail"
 
