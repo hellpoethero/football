b0ae22c56b639ce8fd3eb2b6d6f35490e561d7d6
hellpoethero <hellpoethero1994@gmail.com>
2020-12-28 21:52:25 +0900

    update many things

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ import json
 
 app = Flask(__name__)
 dt = pd.read_csv("https://raw.githubusercontent.com/hellpoethero/football/main/big5.csv", index_col=0)
+# dt = pd.read_csv("big5.csv", index_col=0)
 
 
 @app.route('/')
@@ -29,20 +30,46 @@ def chart(name=None):
 @app.route('/data', methods=["GET"])
 def get_data():
     player_id = request.args.get('player_id')
+    position = request.args.get('position')
+    min_90s = 5
     if player_id:
+        dt_copy = dt.copy()
+        dt_copy["index"] = dt_copy.index
+
         ids = player_id.split(",")
-        player_json = json.loads(dt.iloc[int(ids[0])].to_json())
-        max_json = json.loads(dt[dt["minutes_90s"] >= 5].max().to_json())
+        player_data = dt_copy.loc[int(ids[0])]
+        compare_data = player_data
+        if len(ids) > 1:
+            compare_data = dt_copy.loc[int(ids[1])]
+
+        if position:
+            dt_copy = dt_copy[(dt_copy["position"].str.lower().str.contains(position.lower())) |
+                              (dt_copy["index"] == player_data["index"]) |
+                              (dt_copy["index"] == compare_data["index"])]
+
+        if (player_data["minutes_90s"] >= min_90s) and (compare_data["minutes_90s"] >= min_90s):
+            dt_copy = dt_copy[dt_copy["minutes_90s"] >= min_90s]
+        for col in dt.columns[8:]:
+            dt_copy[col+"_rank"] = np.around((dt_copy[col]).rank(pct=True, na_option='bottom', method='min') * 100)
+
+        player_data = dt_copy.loc[int(ids[0])]
+        player_json = json.loads(player_data.to_json())
         response = {
-            "player": player_json,
-            "max": max_json
+            "success": 1,
+            "player": player_json
         }
         if len(ids) > 1:
-            compare_json = json.loads(dt.iloc[int(ids[1])].to_json())
+            compare_data = dt_copy.loc[int(ids[1])]
+            compare_json = json.loads(compare_data.to_json())
             response["compare"] = compare_json
+
+        max_json = json.loads(dt_copy.max().to_json())
+        response["max"] = max_json
         return response
     else:
-        return "Fail"
+        return {
+            "success": 0
+        }
 
 
 @app.route('/search')
@@ -51,7 +78,7 @@ def search():
     if player_name:
         dt['index'] = dt.index
         cols = ['index','player','nationality','position','squad','age']
-        response = dt[dt["player"].str.lower().str.contains(player_name)][cols].to_json(orient='records')
+        response = dt[dt["player"].str.lower().str.contains(player_name.lower())][cols].to_json(orient='records')
         return response
     else:
         return "Fail"
